3cc271541d91f995672540e1fafef852dc8e4b7b
Maik Schreiber <blizzy@blizzy.de>
2014-05-13 23:31:58 +0200

    prepare for release 1.1.1

--- VoiceCommander/Internal/VoiceCommander.cs
+++ VoiceCommander/Internal/VoiceCommander.cs
@@ -37,7 +37,7 @@ using Toolbar;
 namespace VoiceCommander {
 	[KSPAddonFixed(KSPAddon.Startup.MainMenu, true, typeof(VoiceCommander))]
 	public partial class VoiceCommander : MonoBehaviour {
-		internal const int VERSION = 2;
+		internal const int VERSION = 3;
 
 		private static readonly string SETTINGS_FILE = KSPUtil.ApplicationRootPath + "GameData/blizzy/VoiceCommander/settings.dat";
 		private const string HOST = "127.0.0.1";
